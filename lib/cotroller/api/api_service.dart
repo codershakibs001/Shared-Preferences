cc1ea2ca58725b0301856811c8076e589624dfbc
ShakibProgrammar <shakib501886@gmail.com>
2025-04-04 17:27:30 +0600

    controller folder

--- lib/cotroller/api/api_service.dart
+++ lib/cotroller/api/api_service.dart
@@ -1,13 +1,3 @@
 class ApiService{
 static const String text ="shakib mohammad abdullah al shakib,how are you?you can't hard work";
-
-static const String text2 = "shakib mohammad abdullah al shakib,how are you?you can't hard work";
-static const String text3 = "shakib mohammad abdullah al shakib,how are you?you can't hard work";
-static const String text4 = "shakib mohammad abdullah al shakib,how are you?you can't hard work";
-//         title: Text(text),
-//         subtitle: Text(text2),
-
-//         trailing: Text(text3),
-//         leading: Text(text4),
-//       ),
 }
